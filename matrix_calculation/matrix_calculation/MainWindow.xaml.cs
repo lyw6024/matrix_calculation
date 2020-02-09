cbb5193d79bd194083c8326c6524cca0245cdacf
malic <m6024m@163.com>
2020-02-09 22:05:09 +0800

    LaTeX formulation supported

--- matrix_calculation/matrix_calculation/MainWindow.xaml.cs
+++ matrix_calculation/matrix_calculation/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using WpfMath;
 
 namespace matrix_calculation
 {
@@ -20,9 +23,120 @@ namespace matrix_calculation
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<rationalNumber> numList = new List<rationalNumber>();
+        List<rationalNumber> vectList = new List<rationalNumber>();
+        TexFormulaParser parser = new TexFormulaParser();
+        string left_LaTeX, right_LaTeX;
         public MainWindow()
         {
             InitializeComponent();
+        }
+       
+        private void Mat_Text_KeyUp(object sender, KeyEventArgs e)
+        {
+            left_LaTeX= "";
+            try
+            {
+                var lines = Mat_Text.Text.Split('\n');
+
+                left_LaTeX += @"\pmatrix{";
+                for (int k = 0; k < lines.Length; k++)
+                {
+                    if (k > 0)
+                    {
+                        left_LaTeX += @"\\";
+                    }
+                    var msg = lines[k].Split(' ');
+                    for (int i = 0; i < msg.Length; i++)
+                    {
+                        if (i > 0)
+                        {
+                            left_LaTeX += " & ";
+                        }
+                        rationalNumber a;
+                        if (msg[i].Contains('/'))
+                        {
+                            var rat = msg[i].Split('/');
+                            a = new rationalNumber(Convert.ToInt32(rat[0]), Convert.ToInt32(rat[1]));
+                        }
+                        else
+                        {
+                            a = new rationalNumber(Convert.ToInt32(msg[i]));
+                        }
+                        numList.Add(a);
+                        left_LaTeX += a.ToLaTeX();
+                    }
+
+                }
+
+                left_LaTeX += "}";
+            
+                var formula = parser.Parse(left_LaTeX+"X="+right_LaTeX);
+                var pngBytes = formula.RenderToPng(20.0, 0.0, 0.0, "Arial");
+                MemoryStream ms = new MemoryStream(pngBytes, true);
+
+                BitmapImage p1 = new BitmapImage();
+                p1.BeginInit();
+                p1.StreamSource = ms;
+                p1.EndInit();
+                imgSrc.Source = p1;
+            }
+            catch
+            {
+                ;
+            }
+        }
+
+        private void Vect_Text_KeyUp(object sender, KeyEventArgs e)
+        {
+
+
+            try
+            {
+                vectList.Clear();
+                var lines = Vect_Text.Text.Split('\n');
+                foreach (var line in lines)
+                {
+                    var msg = line.Split(' ');
+                    foreach(var it in msg)
+                    {
+                        rationalNumber a;
+                        if (it.Contains('/'))
+                        {
+                            var rat = it.Split('/');
+                            a = new rationalNumber(Convert.ToInt32(rat[0]), Convert.ToInt32(rat[1]));
+                        }
+                        else
+                        {
+                            a = new rationalNumber(Convert.ToInt32(it));
+                        }
+                        vectList.Add(a);
+                    }
+                }
+                right_LaTeX = @"\pmatrix{";
+                for (int i=0;i<vectList.Count;i++)
+                {
+                    if(i>0)
+                    {
+                        right_LaTeX += @"\\";
+                    }
+                    right_LaTeX += vectList[i].ToLaTeX();
+                }
+                right_LaTeX += "}";
+                var formula = parser.Parse(left_LaTeX+"X="+right_LaTeX);
+                var pngBytes = formula.RenderToPng(20.0, 0.0, 0.0, "Arial");
+                MemoryStream ms = new MemoryStream(pngBytes, true);
+
+                BitmapImage p1 = new BitmapImage();
+                p1.BeginInit();
+                p1.StreamSource = ms;
+                p1.EndInit();
+                imgSrc.Source = p1;
+            }
+            catch
+            {
+                ;
+            }
         }
     }
 }
---
+++ matrix_calculation/matrix_calculation/rationalNumber.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrix_calculation
+{
+    class rationalNumber
+    {
+        private int _num, _den;
+        private int gcd(int x,int y)
+        {
+            if(x%y==0)
+            {
+                return y;
+            }
+            else
+            {
+                return gcd(y, x % y);
+            }
+        }
+
+        public rationalNumber()
+        {
+            _num = 0;
+            _den = 1;
+        }
+        public rationalNumber(int x)
+        {
+            _num = x;
+            _den = 1;
+        }
+        public rationalNumber(int nn,int dd)
+        {
+            int sign = 1;
+            int g = gcd(Math.Abs(nn), Math.Abs(dd));
+            if(nn*dd<0)
+            {
+                sign = -1;
+            }
+            _num = sign*Math.Abs(nn)/g;
+            _den = Math.Abs(dd)/g;
+        }
+        public string ToLaTeX()
+        {
+            if (_den == 1)
+            {
+                return _num.ToString();
+            }
+            else
+            {
+                if (_num > 0)
+                {
+                    return String.Format("\\frac{{{0}}}{{{1}}}", _num, _den);
+                }
+                else
+                {
+                    return String.Format("-\\frac{{{0}}}{{{1}}}", -_num, _den);
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return String.Format("{0}/{1}", _num, _den);
+        }
+        public static rationalNumber operator+(rationalNumber self,rationalNumber o)
+        {
+            return new rationalNumber(self._num * o._den + self._den * o._num, self._den * o._den);
+        }
+        public static rationalNumber operator -(rationalNumber self, rationalNumber o)
+        {
+            return new rationalNumber(self._num * o._den - self._den * o._num, self._den * o._den);
+        }
+        public static rationalNumber operator *(rationalNumber self, rationalNumber o)
+        {
+            return new rationalNumber(self._num * o._num, self._den * o._den);
+        }
+        public static rationalNumber operator /(rationalNumber self, rationalNumber o)
+        {
+            return new rationalNumber(self._num * o._den, self._den * o._num);
+        }
+    }
+}
